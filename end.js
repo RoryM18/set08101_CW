46d5afd746eebdb2e0b8ea51ff1c271f653442b7
Rory <40492068@live.napier.ac.uk>
2022-04-22 17:30:30 +0100

    Changed disable

--- end.js
+++ end.js
@@ -3,7 +3,7 @@ const saveScoreBtn = documnet.querySelector('#saveScoreBtn')
 const finalScore = documnet.querySelector('#finalScore')
 const mostRecentScore = localStorage.getItem('mostRecentScore')
 
-const highScore = JSON.parse(localStorage.getItem('highScore')) || []
+const highScores = JSON.parse(localStorage.getItem('highScore')) || []
 
 const MAX_HIGH_SCORE = 5
 
@@ -21,14 +21,14 @@ saveHighScore = e => {
         name: username.value
     }
 
-    highScore.push(score)
+    highScores.push(score)
 
-    highScore.sort((a,b) => {
+    highScores.sort((a,b) => {
         return b.score - a.score
     })
 
-    highScore.splice(5)
+    highScores.splice(5)
 
-    localStorage.setItem('highScore', JSON.stringify(highScore))
+    localStorage.setItem('highScore', JSON.stringify(highScores))
     window.location.assign('/')
 }
